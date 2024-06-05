0f0200a50dfd2fb607d49d2f2674d3456a49751a
KONFeature <quentin@nivelais.com>
2024-06-05 10:12:21 +0200

    🐛 Test with a container name

--- sst.config.ts
+++ sst.config.ts
@@ -112,10 +112,11 @@ function IndexerStack({ app, stack }: StackContext) {
             },
             // Directly specify the image position in the registry here
             container: {
+                containerName: "indexer",
                 image: indexerImage,
                 secrets: cdkSecretsMap,
                 // Mark the container as non-essential, to don't fail the whole CI in case of deployment bug
-                // essential: false,
+                essential: false,
             },
         },
     });
