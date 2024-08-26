e29c7610e18a63446f1c9e95cd361e373c09d903
KONFeature <quentin@nivelais.com>
2024-08-26 10:58:01 +0200

    ✨ Add api endpoint for interactions history

---
+++ packages/ponder/src/api/interactions.ts
@@ -0,0 +1,43 @@
+import { ponder } from "@/generated";
+import { desc, eq } from "@ponder/core";
+import { type Address, isAddress } from "viem";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: Unreachable code error
+BigInt.prototype.toJSON = function (): string {
+    return this.toString();
+};
+
+/**
+ * Get all the interactions for a wallet
+ */
+ponder.get("/interactions/:wallet", async (ctx) => {
+    // Extract wallet
+    const wallet = ctx.req.param("wallet") as Address;
+    if (!isAddress(wallet)) {
+        return ctx.text("Invalid wallet address", 400);
+    }
+
+    // Get the tables we will query
+    const { InteractionEvent, ContentInteractionContract } = ctx.tables;
+
+    // Perform the sql query
+    const contents = await ctx.db
+        .select({
+            data: InteractionEvent.data,
+            type: InteractionEvent.type,
+            timestamp: InteractionEvent.timestamp,
+            contentId: ContentInteractionContract.contentId,
+        })
+        .from(InteractionEvent)
+        .innerJoin(
+            ContentInteractionContract,
+            eq(ContentInteractionContract.id, InteractionEvent.interactionId)
+        )
+        .where(eq(InteractionEvent.user, wallet))
+        .limit(100)
+        .orderBy(desc(InteractionEvent.timestamp));
+
+    // Return the result as json
+    return ctx.json(contents);
+});
